877303dd203385797c23f5f86c0f0189233a9668
ale99318 <jesusjironrodriguez155@gmail.com>
2025-05-19 23:51:27 -0500

    Create jugadores.js

---
+++ jugadores.js
@@ -0,0 +1,115 @@
+const jugadores = [
+  // Alianza Lima
+  {
+    nombre: "Hernán Barcos",
+    club: "Alianza Lima",
+    posicion: "Delantero",
+    sprint: 72,
+    regate: 70,
+    pase: 68,
+    general: 74,
+    valor: 1500000
+  },
+  {
+    nombre: "Pablo Sabbag",
+    club: "Alianza Lima",
+    posicion: "Delantero",
+    sprint: 75,
+    regate: 67,
+    pase: 69,
+    general: 73,
+    valor: 1300000
+  },
+
+  // Universitario
+  {
+    nombre: "Álex Valera",
+    club: "Universitario",
+    posicion: "Delantero",
+    sprint: 74,
+    regate: 66,
+    pase: 67,
+    general: 72,
+    valor: 1250000
+  },
+  {
+    nombre: "Emanuel Herrera",
+    club: "Universitario",
+    posicion: "Delantero",
+    sprint: 70,
+    regate: 65,
+    pase: 67,
+    general: 71,
+    valor: 1200000
+  },
+
+  // Sporting Cristal
+  {
+    nombre: "Ignacio da Silva",
+    club: "Sporting Cristal",
+    posicion: "Defensa",
+    sprint: 68,
+    regate: 60,
+    pase: 65,
+    general: 70,
+    valor: 1100000
+  },
+  {
+    nombre: "Brenner Marlos",
+    club: "Sporting Cristal",
+    posicion: "Delantero",
+    sprint: 78,
+    regate: 72,
+    pase: 70,
+    general: 75,
+    valor: 1600000
+  },
+
+  // Melgar
+  {
+    nombre: "Bernardo Cuesta",
+    club: "Melgar",
+    posicion: "Delantero",
+    sprint: 74,
+    regate: 70,
+    pase: 67,
+    general: 73,
+    valor: 1400000
+  },
+  {
+    nombre: "Alec Deneumostier",
+    club: "Melgar",
+    posicion: "Defensa",
+    sprint: 68,
+    regate: 62,
+    pase: 64,
+    general: 69,
+    valor: 1100000
+  },
+
+  // Cienciano
+  {
+    nombre: "Danilo Carando",
+    club: "Cienciano",
+    posicion: "Delantero",
+    sprint: 71,
+    regate: 66,
+    pase: 64,
+    general: 70,
+    valor: 1200000
+  },
+  {
+    nombre: "Hansell Riojas",
+    club: "Cienciano",
+    posicion: "Defensa",
+    sprint: 67,
+    regate: 60,
+    pase: 63,
+    general: 68,
+    valor: 1000000
+  }
+
+  // Puedes continuar con los demás clubes aquí...
+];
+
+window.jugadores = jugadores;
