5197ae33da68322c0a5fbe4f64271cb97d441dc6
ale99318 <jesusjironrodriguez155@gmail.com>
2025-05-20 17:03:27 -0500

    Create clubes_compradores.js

---
+++ clubes_compradores.js
@@ -0,0 +1,89 @@
+const clubesCompradores = [
+  // Clubes peruanos
+  {
+    nombre: "Sporting Cristal",
+    pais: "Perú",
+    presupuesto: 1200000,
+    reputacion: 85,
+  },
+  {
+    nombre: "FBC Melgar",
+    pais: "Perú",
+    presupuesto: 900000,
+    reputacion: 78,
+  },
+  {
+    nombre: "Cienciano",
+    pais: "Perú",
+    presupuesto: 750000,
+    reputacion: 72,
+  },
+  {
+    nombre: "UTC Cajamarca",
+    pais: "Perú",
+    presupuesto: 500000,
+    reputacion: 65,
+  },
+  {
+    nombre: "Cusco FC",
+    pais: "Perú",
+    presupuesto: 600000,
+    reputacion: 68,
+  },
+
+  // Clubes extranjeros
+  {
+    nombre: "Colón",
+    pais: "Argentina",
+    presupuesto: 1500000,
+    reputacion: 82,
+  },
+  {
+    nombre: "Deportivo Cali",
+    pais: "Colombia",
+    presupuesto: 1400000,
+    reputacion: 80,
+  },
+  {
+    nombre: "Nacional",
+    pais: "Uruguay",
+    presupuesto: 2000000,
+    reputacion: 90,
+  },
+  {
+    nombre: "Oriente Petrolero",
+    pais: "Bolivia",
+    presupuesto: 700000,
+    reputacion: 70,
+  },
+  {
+    nombre: "Barcelona SC",
+    pais: "Ecuador",
+    presupuesto: 1800000,
+    reputacion: 88,
+  },
+  {
+    nombre: "Olimpia",
+    pais: "Paraguay",
+    presupuesto: 1600000,
+    reputacion: 85,
+  },
+  {
+    nombre: "Tigre",
+    pais: "Argentina",
+    presupuesto: 1100000,
+    reputacion: 77,
+  },
+  {
+    nombre: "Huachipato",
+    pais: "Chile",
+    presupuesto: 950000,
+    reputacion: 74,
+  },
+  {
+    nombre: "The Strongest",
+    pais: "Bolivia",
+    presupuesto: 850000,
+    reputacion: 73,
+  }
+];
