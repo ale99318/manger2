6b7baecb88fbedc7d81355658fe5d592570d17c8
ale99318 <jesusjironrodriguez155@gmail.com>
2025-05-19 23:01:54 -0500

    Create clubes.js

---
+++ clubes.js
@@ -0,0 +1,128 @@
+const clubes = [
+  {
+    nombre: "Alianza Lima",
+    presupuesto: 12000000,
+    clasico: "Universitario",
+    estadio: "Estadio Alejandro Villanueva",
+    ciudad: "Lima"
+  },
+  {
+    nombre: "Universitario",
+    presupuesto: 11000000,
+    clasico: "Alianza Lima",
+    estadio: "Estadio Monumental",
+    ciudad: "Lima"
+  },
+  {
+    nombre: "Sporting Cristal",
+    presupuesto: 9500000,
+    clasico: "Universitario",
+    estadio: "Estadio Alberto Gallardo",
+    ciudad: "Lima"
+  },
+  {
+    nombre: "Melgar",
+    presupuesto: 8000000,
+    clasico: "Cienciano",
+    estadio: "Estadio Monumental de la UNSA",
+    ciudad: "Arequipa"
+  },
+  {
+    nombre: "Cienciano",
+    presupuesto: 7500000,
+    clasico: "Melgar",
+    estadio: "Estadio Inca Garcilaso de la Vega",
+    ciudad: "Cusco"
+  },
+  {
+    nombre: "Cusco FC",
+    presupuesto: 5000000,
+    clasico: "Ninguno",
+    estadio: "Estadio Inca Garcilaso de la Vega",
+    ciudad: "Cusco"
+  },
+  {
+    nombre: "ADT",
+    presupuesto: 4000000,
+    clasico: "Ninguno",
+    estadio: "Estadio Unión Tarma",
+    ciudad: "Tarma"
+  },
+  {
+    nombre: "Sport Boys",
+    presupuesto: 4200000,
+    clasico: "Municipal",
+    estadio: "Estadio Miguel Grau",
+    ciudad: "Callao"
+  },
+  {
+    nombre: "Deportivo Municipal",
+    presupuesto: 4500000,
+    clasico: "Sport Boys",
+    estadio: "Estadio Iván Elías Moreno",
+    ciudad: "Lima"
+  },
+  {
+    nombre: "UTC",
+    presupuesto: 3500000,
+    clasico: "Ninguno",
+    estadio: "Estadio Héroes de San Ramón",
+    ciudad: "Cajamarca"
+  },
+  {
+    nombre: "Carlos A. Mannucci",
+    presupuesto: 4300000,
+    clasico: "César Vallejo",
+    estadio: "Estadio Mansiche",
+    ciudad: "Trujillo"
+  },
+  {
+    nombre: "César Vallejo",
+    presupuesto: 4800000,
+    clasico: "Carlos A. Mannucci",
+    estadio: "Estadio Mansiche",
+    ciudad: "Trujillo"
+  },
+  {
+    nombre: "Sport Huancayo",
+    presupuesto: 5000000,
+    clasico: "Ninguno",
+    estadio: "Estadio Huancayo",
+    ciudad: "Huancayo"
+  },
+  {
+    nombre: "Alianza Atlético",
+    presupuesto: 3800000,
+    clasico: "Atlético Grau",
+    estadio: "Estadio Campeones del 36",
+    ciudad: "Sullana"
+  },
+  {
+    nombre: "Atlético Grau",
+    presupuesto: 4600000,
+    clasico: "Alianza Atlético",
+    estadio: "Estadio Municipal de Bernal",
+    ciudad: "Piura"
+  },
+  {
+    nombre: "Unión Comercio",
+    presupuesto: 3200000,
+    clasico: "Ninguno",
+    estadio: "Estadio Carlos Vidaurre García",
+    ciudad: "Tarapoto"
+  },
+  {
+    nombre: "Los Chankas",
+    presupuesto: 3000000,
+    clasico: "Ninguno",
+    estadio: "Estadio Los Chankas",
+    ciudad: "Andahuaylas"
+  },
+  {
+    nombre: "Comerciantes Unidos",
+    presupuesto: 3100000,
+    clasico: "Ninguno",
+    estadio: "Estadio Juan Maldonado Gamarra",
+    ciudad: "Cutervo"
+  }
+];
