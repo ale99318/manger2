0cfc1f301b1dd8c595ba7b98a76c4f4767630495
ale99318 <jesusjironrodriguez155@gmail.com>
2025-05-24 20:47:49 -0500

    Update clubes.js

--- clubes.js
+++ clubes.js
@@ -1,5 +1,6 @@
 const clubes = [ 
   {
+    id: "51-1".
     nombre: "Alianza Lima",
     presupuesto: 12000000,
     clasico: "Universitario",
@@ -7,6 +8,7 @@ const clubes = [
     ciudad: "Lima"
   },
   {
+    id: "51-2".
     nombre: "Universitario",
     presupuesto: 11000000,
     clasico: "Alianza Lima",
@@ -14,6 +16,7 @@ const clubes = [
     ciudad: "Lima"
   },
   {
+    id: "51-3".
     nombre: "Sporting Cristal",
     presupuesto: 9500000,
     clasico: "Universitario",
@@ -21,6 +24,7 @@ const clubes = [
     ciudad: "Lima"
   },
   {
+    id: "51-4".
     nombre: "Melgar",
     presupuesto: 8000000,
     clasico: "Cienciano",
@@ -28,6 +32,7 @@ const clubes = [
     ciudad: "Arequipa"
   },
   {
+    id: "51-5".
     nombre: "Cienciano",
     presupuesto: 7500000,
     clasico: "Melgar",
@@ -35,6 +40,7 @@ const clubes = [
     ciudad: "Cusco"
   },
   {
+    id: "51-6".
     nombre: "Cusco FC",
     presupuesto: 5000000,
     clasico: "Ninguno",
@@ -42,6 +48,7 @@ const clubes = [
     ciudad: "Cusco"
   },
   {
+    id: "51-7".
     nombre: "ADT",
     presupuesto: 4000000,
     clasico: "Ninguno",
@@ -49,6 +56,7 @@ const clubes = [
     ciudad: "Tarma"
   },
   {
+    id: "51-8".
     nombre: "Sport Boys",
     presupuesto: 4200000,
     clasico: "Municipal",
@@ -56,6 +64,7 @@ const clubes = [
     ciudad: "Callao"
   },
   {
+    id: "51-9".
     nombre: "Deportivo Binacional Fútbol Club",
     presupuesto: 4500000,
     clasico: "Ninguno",
@@ -63,6 +72,7 @@ const clubes = [
     ciudad: "Juliaca"
   },
   {
+    id: "51-10".
     nombre: "UTC",
     presupuesto: 3500000,
     clasico: "Ninguno",
@@ -70,6 +80,7 @@ const clubes = [
     ciudad: "Cajamarca"
   },
   {
+    id: "51-11".
     nombre: "Carlos A. Mannucci",
     presupuesto: 4300000,
     clasico: "César Vallejo",
@@ -77,6 +88,7 @@ const clubes = [
     ciudad: "Trujillo"
   },
   {
+    id: "51-12".
     nombre: "Club Deportivo Garcilaso",
     presupuesto: 4800000,
     clasico: "Cienciano",
@@ -84,6 +96,7 @@ const clubes = [
     ciudad: "Cusco"
   },
   {
+    id: "51-13".
     nombre: "Sport Huancayo",
     presupuesto: 5000000,
     clasico: "Ninguno",
@@ -91,6 +104,7 @@ const clubes = [
     ciudad: "Huancayo"
   },
   {
+    id: "51-14".
     nombre: "Alianza Atlético",
     presupuesto: 3800000,
     clasico: "Atlético Grau",
@@ -98,6 +112,7 @@ const clubes = [
     ciudad: "Sullana"
   },
   {
+    id: "51-15".
     nombre: "Atlético Grau",
     presupuesto: 4600000,
     clasico: "Alianza Atlético",
@@ -105,6 +120,7 @@ const clubes = [
     ciudad: "Piura"
   },
   {
+    id: "51-16".
     nombre: "Juan Pablo II College",
     presupuesto: 3200000,
     clasico: "Ninguno",
@@ -112,6 +128,7 @@ const clubes = [
     ciudad: "Tarapoto"
   },
   {
+    id: "51-17".
     nombre: "Los Chankas",
     presupuesto: 3000000,
     clasico: "Ninguno",
@@ -119,6 +136,7 @@ const clubes = [
     ciudad: "Andahuaylas"
   },
   {
+    id: "51-18".
     nombre: "Comerciantes Unidos",
     presupuesto: 3100000,
     clasico: "Ninguno",
